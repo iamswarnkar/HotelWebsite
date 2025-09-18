976fe7a8703cec13cec6d0116f6311c4fe792aa3
iamswarnkar <iamswarnkar7@gmail.com>
2025-09-18 21:49:35 +0530

    feat:date

--- App.js
+++ App.js
@@ -1,74 +1,78 @@
-
 // Header Scroll
 let nav = document.querySelector(".navbar");
 window.onscroll = function () {
-    if (document.documentElement.scrollTop > 250) {
-        nav.classList.add("header-scrolled");
-    } else {
-        nav.classList.remove("header-scrolled");
-    }
-}
+  if (document.documentElement.scrollTop > 250) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
 
-// nav hide 
+// nav hide
 let navBar = document.querySelectorAll(".nav-link");
-let bar = document.querySelector('#bar');
+let bar = document.querySelector("#bar");
 let navCollapse = document.querySelector(".navbar-collapse.collapse");
 
 navBar.forEach(function (e) {
-    e.addEventListener("click", function () {
-        navCollapse.classList.remove("show");
-        if(bar.classList[2] === 'fa-times'){
-            bar.classList.replace('fa-times','fa-bars');
-        }
-    })
-})
-
-bar.addEventListener('click',()=>{
-    if(bar.classList[2] === 'fa-bars'){
-        bar.classList.replace('fa-bars','fa-times');
-    }else{
-        bar.classList.replace('fa-times','fa-bars');
+  e.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+    if (bar.classList[2] === "fa-times") {
+      bar.classList.replace("fa-times", "fa-bars");
     }
- 
-}); 
+  });
+});
+
+bar.addEventListener("click", () => {
+  if (bar.classList[2] === "fa-bars") {
+    bar.classList.replace("fa-bars", "fa-times");
+  } else {
+    bar.classList.replace("fa-times", "fa-bars");
+  }
+});
 
 // Swiper Slider
 var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3500,
-    },
+  direction: "vertical",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3500,
+  },
 });
-
 
 // Our Partner
 var swiper = new Swiper(".our-partner", {
-    slidesPerView: 5,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: {
-        delay: 2000,
+  slidesPerView: 5,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  breakpoints: {
+    991: {
+      slidesPerView: 5,
+      spaceBetween: 10,
     },
-    breakpoints: {
-        '991': {
-            slidesPerView: 5,
-            spaceBetween: 10,
-        },
-        '767': {
-            slidesPerView: 3,
-            spaceBetween: 10,
-        },
-        '320': {
-            slidesPerView: 2,
-            spaceBetween: 8,
-        },
-
-
+    767: {
+      slidesPerView: 3,
+      spaceBetween: 10,
     },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+  },
 });
 
+document.querySelectorAll('input[type="date"]').forEach(function (dateInput) {
+  dateInput.addEventListener("change", function () {
+    if (this.value) {
+      this.classList.add("has-value");
+    } else {
+      this.classList.remove("has-value");
+    }
+  });
+});
